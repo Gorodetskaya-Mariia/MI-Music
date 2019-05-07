3e2810939a86ad1f7a009e7987b04f467f5179a3
Mariia <1907mss@gmail.com>
2019-05-08 00:02:21 +0300

    animated menu is added

--- build/js/scripts.min.js
+++ build/js/scripts.min.js
@@ -1,54 +1,47 @@
 (function () {
-  document.querySelector('.button__menu').addEventListener('click', function() {
-    this.classList.toggle('active');
-    document.querySelector('.hamburger').classList.toggle('is-open');
-    document.querySelector('.menu__list--nav').toggleClass('is-open');
-  })
+  let menuBtn = document.querySelector('.button__menu'),
+      mainNav = document.querySelector('.menu__list--nav'),
+      hamburger = document.querySelector('.hamburger'),
+      widthHeaderMenu = mainNav.clientWidth;
 
-  (function () {
+      mainNav.style.right = -(widthHeaderMenu + 130) + 'px';
+
+  menuBtn.onclick = function () {
+    menuBtn.classList.toggle('is-open');
+    hamburger.classList.toggle('is-open');
+    mainNav.classList.toggle('is-open');
+    if(mainNav.classList.contains('is-open')){
+      mainNav.style.right = '0';
+    } else {
+      mainNav.style.right = -(widthHeaderMenu + 130) + 'px';
+    }
+
+  }
+
+  // (function () {
     //emergence of an hamburger
-
     window.addEventListener('scroll', () => {
         let nav = document.querySelector('.header__nav-list'),
             height = document.querySelector('header').offsetHeight,
             navToggle = document.querySelector('.button__menu'),
-            mainNav = document.querySelector('.main__nav-list'),
-            shadowActiveClassName = 'active',
             navToggleActiveClass = 'active',
             viewport = document.body.clientWidth;
 
 
-        if (viewport < 1279) {
             if (window.pageYOffset >= height) {
                 burgerToggleAppearance('SHOW', navToggle, navToggleActiveClass)
+                burgerToggleAppearance('SHOW', mainNav, navToggleActiveClass)
+                // mainNav.style.right = -(widthHeaderMenu + 30) + 'px';
 
             } else {
                 burgerToggleAppearance('HIDE', navToggle, navToggleActiveClass)
-                mainNav.classList.remove('active')
-
+                burgerToggleAppearance('HIDE', navToggle, 'is-open')
+                burgerToggleAppearance('HIDE', mainNav, navToggleActiveClass)
+                burgerToggleAppearance('HIDE', mainNav, 'is-open')
+                burgerToggleAppearance('HIDE', hamburger, 'is-open')
             }
-
-        }  else {
-            if (window.pageYOffset >= height / 2 && window.pageYOffset <= height) {
-                nav.classList.add(shadowClassName);
-                nav.classList.add("active")
-            } else {
-                nav.classList.remove(shadowClassName)
-                nav.classList.remove("active")
-            }
-
-            if (window.pageYOffset >= height) {
-                burgerToggleAppearance('SHOW', navToggle, navToggleActiveClass)
-
-            } else {
-                burgerToggleAppearance('HIDE', navToggle, navToggleActiveClass)
-                mainNav.classList.remove('active')
-                nav.classList.remove(shadowActiveClassName)
-            }
-        }
-
     })
-  })();
+  // })();
 })();
 
 function burgerToggleAppearance(state, nav, activeClass) {
